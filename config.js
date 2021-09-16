959f1b2e5a2753a20929dc16e00d47a604657846
Francesca Marini <48101104+francesca418@users.noreply.github.com>
2021-09-16 02:41:20 -0400

    first files for new website

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'Francesca Marini', // <title>
+  manifestName: 'francesca-marini-website',
+  manifestShortName: 'Francesca Marini', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399', // '#BB96F9',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/francesca-marini/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Francesca Marini',
+  subHeading: 'Welcome to my site.',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-envelope',
+      name: 'Email',
+      url: 'mailto:fmarini@seas.upenn.edu',
+    },
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/francesca418',
+    },
+    {
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/francescammarini/',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://www.facebook.com/profile.php?id=100006076810158',
+    },
+  ],
+  emailId: 'fmarini@seas.upenn.edu',
+};
---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+const config = require('./config');
+
+module.exports = {
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    title: config.siteTitle,
+    siteUrl: `https://www.example.com`,
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.manifestName,
+        short_name: config.manifestShortName,
+        start_url: config.pathPrefix || config.manifestStartUrl,
+        background_color: config.manifestBackgroundColor,
+        theme_color: config.manifestThemeColor,
+        display: config.manifestDisplay,
+        icon: config.manifestIcon, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+    //'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
+    /*{
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'config/typography.js',
+      },
+    },*/
+    {
+    //resolve: 'gatsby-plugin-emotion',
+    options: {
+      autoLabel: process.env.NODE_ENV !== 'production',
+      // eslint-disable-next-line
+      labelFormat: `[filename]--[local]`,
+    },
+  },
+  ],
+};
